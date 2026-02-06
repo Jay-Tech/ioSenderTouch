10fc7ba7b7e366a35219f1d01867a45bcdff4c91
Jay-Tech <Jayh160@yahoo.com>
2026-02-05 20:37:18 -0600

    name change

--- ioSender Touch/GrblHalSender/MainWindow.xaml.cs
+++ ioSender Touch/GrblHalSender/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ namespace GrblHalSender
     public partial class MainWindow : Window
     {
         private const string Version = "2.0.4";
-        private const string App_Name = "IO Sender Touch";
+        private const string App_Name = "GrblHal Sender";
 
         private readonly GrblViewModel _viewModel;
         private readonly HomeView _homeView;
